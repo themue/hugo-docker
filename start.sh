b5d11dafc8ee4ff452baf3441deb309376e82001
Frank Mueller <mue@tideland.biz>
2017-03-10 23:00:46 +0100

    Re-added bind

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ SH=/bin/sh
 PULL=/pull.sh
 HUGO=/go/bin/hugo
 HUGO_SOURCE="--source=/src"
-HUGO_DESTINATION="--destination=/var/www/site"
-HUGO_ARGS="server $HUGO_SOURCE $HUGO_DESTINATION $@"
+HUGO_DESTINATION="--destination=/site"
+HUGO_BIND="--bind=0.0.0.0"
+HUGO_ARGS="server -v $HUGO_SOURCE $HUGO_DESTINATION $HUGO_BIND $@"
 
 $SH $PULL &
 $HUGO $HUGO_ARGS || exit 1
